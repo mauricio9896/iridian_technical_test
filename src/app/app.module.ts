c2d252b761c88c8dd18f302a4d5b05c9aaeaaa44
Mauricio Buitrago <mauricio9896@hotmail.com>
2024-05-28 14:53:21 -0500

    fixed shared module and market component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,12 @@ import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { HomeComponent } from './components/home/home.component';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
-import { SharedModule } from './shared.module';
 import { CardComponent } from './components/card/card.component';
 import { EventsComponent } from './components/events/events.component';
 import { MarketComponent } from './components/market/market.component';
 import { HttpClientModule } from '@angular/common/http';
 import { BetslipComponent } from './components/betslip/betslip.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
